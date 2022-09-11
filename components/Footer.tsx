9d94e53ab466845adc248799272696bcc787d915
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-09-11 11:38:33 +0200

    ♻️ Update : add footer dynamic options

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,8 @@
+"use client";
 import Image from 'next/image'
-import React from 'react'
+import React from 'react';
+import { footerLinks } from '@/constants';
+import Link from 'next/link';
 
 const Footer = () => {
   return (
@@ -13,7 +16,32 @@ const Footer = () => {
                     All the rights are reserved &copy;
                  </p>
             </div>
+            <div className="footer__links">
+              {footerLinks.map((link) => (
+                <div className='footer__link' key={link.title}>
+                  <h3 className='font-bold'> {link.title}</h3>
+                  {link.links.map((item)=>(
+                    <Link
+                    key={item.title}
+                    href={item.url}
+                    className='text-gray-500' > {item.title} </Link>
+                  ))}
+                </div>
+              ))}
+            </div>
+           
         </div>
+        <div className='flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 py-10 '>
+              <p className=''>@2023 All the Rights are Reserved</p>
+              <div className='footer__copyrights-link'>
+                <Link className="text-gray-500" href='/'>
+                  Privacy Policy 
+                </Link>
+                <Link className="text-gray-500" href='/'>
+                 Term of Use
+                </Link>
+                 </div>
+            </div> 
     </footer>
   )
 }

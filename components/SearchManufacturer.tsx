63b1c088e6504642fdaadea3e15670ca0e7ef123
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2022-09-18 11:40:37 +0200

    🎉 Add : New component for search manufacturer details

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,75 @@
+"use client";
+import Image from 'next/image';
+import {Fragment, useState} from 'react';
+import { Combobox, Transition } from '@headlessui/react';
+import { SearchManufacturerProps } from '@/types';
+import React from 'react'
+import { manufacturers } from '@/constants';
+
+const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
+     const [query, setQuery] = useState('');
+     const filteredManufacturers = query === "" ? manufacturers : manufacturers.filter((item)=>(
+        item.toLocaleLowerCase().replace(/\s+/g, "").includes(query.toLowerCase().replace(/\s+/g, ""))
+     )) 
+return (
+    <div className='search-manufacturer'>
+        <Combobox value={manufacturer} onChange={setManufacturer}>
+            <div className='relative w-full'>
+                <Combobox.Button className="absolute top-[14px]">
+                <Image
+                    src='/car-logo.svg'
+                    width={20}
+                    height={20}
+                    className='ml-4'
+                    alt='car logo'
+                />
+                </Combobox.Button>
+                <Combobox.Input className="search-manufacturer__input" 
+                   placeholder='Volkswagen' 
+                   onChange={(e) => setQuery(e.target.value)}
+                   displayValue={(manufacturer:string) => manufacturer}
+                />
+                <Transition as={Fragment} leaveFrom='opacity-0' 
+                afterLeave={()=> setQuery('')}
+                leave='transition ease-in duration-100'>
+                   <Combobox.Options>
+                    {filteredManufacturers.length === 0 && query !== "" ?  (
+                        <Combobox.Option value={query} className="search-manufacturer__option">
+                            Create "{query}"
+                        </Combobox.Option>
+                    ): 
+                    (filteredManufacturers.map((item) =>(
+                        <Combobox.Option key={item} value={item}
+                         className={({active}) => `relative search-manufacturer__option ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`}> 
+                          
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >  
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+
+                        </Combobox.Option>
+                    )))}
+                   </Combobox.Options>
+                </Transition>
+            </div>
+        </Combobox>
+    </div>
+  )
+}
+
+export default SearchManufacturer

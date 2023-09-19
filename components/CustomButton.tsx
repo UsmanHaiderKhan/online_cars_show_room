106bcb165982dcb879ca2839eb8979a124edb034
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-09-19 17:17:15 +0200

    ♻️ Update : custom button add dynamic params to make it more generic

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,13 +1,28 @@
 "use client";
-import { CustomButton } from '@/types';
-import React from 'react'
 
-const CustomButton = ({title, containerStyles, handleClick}:CustomButton) => {
-  return (
-    <button disabled={false} type={'button'}
-     className={`custom-btn ${containerStyles}`}>
-    <span className={`flex-1`}>{title}</span></button>
-  )
-}
+import Image from "next/image";
+import { CustomButtonProps } from "@/types";
 
-export default CustomButton
+
+const CustomButton = ({ isDisabled, btnType, containerStyles, textStyles, title, rightIcon, handleClick }: CustomButtonProps) => (
+  <button
+    disabled={isDisabled}
+    type={btnType || "button"}
+    className={`custom-btn ${containerStyles}`}
+    onClick={handleClick}
+  >
+    <span className={`flex-1 ${textStyles}`}>{title}</span>
+    {rightIcon && (
+      <div className="relative w-6 h-6">
+        <Image
+          src={rightIcon}
+          alt="arrow_left"
+          fill
+          className="object-contain"
+        />
+      </div>
+    )}
+  </button>
+);
+
+export default CustomButton;

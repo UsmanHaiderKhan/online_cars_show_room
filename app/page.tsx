681c73832be9a4b495d62f3b7ed1512e4dd90788
Usman Haider Khan <37379001+UsmanHaiderKhan@users.noreply.github.com>
2023-10-07 13:08:07 +0200

    ♻️ Update : Pass all the parameters in fetchCars() function

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,15 @@ import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
 import { fetchCars } from '@/utils';
 import Image from 'next/image'; 
 
-export default async function Home() {
-  const allCars = await fetchCars();
+export default async function Home({searchParams}) {
+  const allCars = await fetchCars({
+    manufacturers: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+
+  });
   const result = allCars.data;
   const isDataEmpty = !Array.isArray(result) || result.length < 1 || !result;
   
